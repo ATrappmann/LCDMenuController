a275a8dff25dd0379113c5cf74e15df9f2470339
Trappmann <Andreas.Trappmann@swisslife.de>
2020-04-27 16:08:34 +0200

    Simplied example.

--- examples/SimpleMenu/SimpleMenu.ino
+++ examples/SimpleMenu/SimpleMenu.ino
@@ -8,18 +8,6 @@
 //
 #include "LCDMenuController.h"
 
-#define LCD_RS_PIN  MCP23017_PA7
-#define LCD_RW_PIN  MCP23017_PA6
-#define LCD_EN_PIN  MCP23017_PA5
-#define LCD_D0_PIN  MCP23017_PB0
-#define LCD_D1_PIN  MCP23017_PB1
-#define LCD_D2_PIN  MCP23017_PB2
-#define LCD_D3_PIN  MCP23017_PB3
-#define LCD_D4_PIN  MCP23017_PB4
-#define LCD_D5_PIN  MCP23017_PB5
-#define LCD_D6_PIN  MCP23017_PB6
-#define LCD_D7_PIN  MCP23017_PB7
-#define LCD_BACKLIGHT_PIN MCP23017_PA1
 #define LCD_I2C_ADDR  0x20
 
 #define UP_PIN      5
@@ -56,10 +44,7 @@ Menu mainMenu[] = {
   NULL
 };
 
-LiquidCrystal_MCP23017_I2C lcd = LiquidCrystal_MCP23017_I2C(LCD_I2C_ADDR, LCD_RS_PIN, LCD_RW_PIN, LCD_EN_PIN, LCD_BACKLIGHT_PIN,
-                               LCD_D0_PIN, LCD_D1_PIN, LCD_D2_PIN, LCD_D3_PIN,
-                               LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
-
+LiquidCrystal_MCP23017_I2C lcd = LiquidCrystal_MCP23017_I2C(LCD_I2C_ADDR);
 LCDMenuController controller = LCDMenuController(&lcd, 16, 2, DOWN_PIN, UP_PIN, SELECT_PIN, BACK_PIN);
 
 void setup() {
